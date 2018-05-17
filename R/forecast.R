162039bf994e7352cb141ebed78a41e3705d72ef
elisvb <elisvb@hotmail.com>
2018-05-17 11:44:26 -0400

    correct roxygen

--- R/forecast.R
+++ R/forecast.R
@@ -30,20 +30,23 @@ rmvnorm <- function(n = 1, mu, Sigma){
 ##' @param fscale a vector of f-scales. See details.
 ##' @param catchval a vector of target catches. See details.
 ##' @param fval a vector of target f values. See details.
+##' @param MP a vector of management strategie names. see avail('MP')
 ##' @param nosim number of simulations default is 1000
 ##' @param year.base starting year default last year in assessment. Currently it is only supported to use last assessment year or the year before
 ##' @param ave.years vector of years to average for weights, maturity, M and such
 ##' @param rec.years vector of years to use to resample recruitment from
+##' @param rec.meth methode to project recruitment
 ##' @param label optional label to appear in short table
 ##' @param overwriteSelYears if a vector of years is specified, then the average selectivity of those years is used (not recommended)
 ##' @param deterministic option to turn all process noise off (not recommended, as it will likely cause bias)
 ##' @param CZ critical zone (to calculate probability ssb above)
 ##' @param HZ Healty zone (to calculate probability ssb above)
-##' @param IE implementation error (list of IE per time step)
+##' @param IE implementation error (vector). See avail('IE')
 ##' @param capLower lower limit of TAC (before IE)
 ##' @param capUpper upper limit of TAC (before IE)
+##' @param UL.years vector of years to use to resample upper/lower catch bound ranges from
 ##' @param TAC.base TAC last year
-##' @details There are three ways to specify a scenario. If e.g. four F values are specified (e.g. fval=c(.1,.2,.3,4)), then the first value is used in the last assessment year (base.year), and the three following in the three following years. Alternatively F's can be specified by a scale, or a target catch. Only one option can be used per year. So for instance to set a catch in the first year and an F-scale in the following one would write catchval=c(10000,NA,NA,NA), fscale=c(NA,1,1,1). The length of the vector specifies how many years forward the scenarios run.
+##' @details There are four ways to specify a scenario; fscale, catchval, fval and MP.
 ##' @return an object of type ccamforecast
 ##' @importFrom stats median uniroot quantile
 ##' @export
