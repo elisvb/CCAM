3ae533323534c2094581255343b5c4cab543872a
elisvb <elisvb@hotmail.com>
2018-05-17 11:27:26 -0400

    Delete debug.R

--- test/debug.R
+++
@@ -1,9 +0,0 @@
-fixwinpath <- function() {
-    PATH <- Sys.getenv("PATH")
-    PATH <- paste0(R.home(), "/bin/x64;", PATH)
-    PATH <- paste0("c:/Rtools/mingw_64/bin;", PATH)
-    Sys.setenv(PATH=PATH)
-}
-fixwinpath()
-setwd("C:/Users/VANBE/Desktop/post-doc/DATA/CCAM/test/")
-TMB :: gdbsource ('CCAM.r' , interactive = TRUE )
